2797b3dd87d4d02c2026c18312870be828a63ccb
ron93 <ronmkare1@gmail.com>
2021-02-14 20:50:55 +0300

    save to csv

--- Praw/praw.py
+++ Praw/praw.py
@@ -54,7 +54,7 @@ class SubredditScrapper:
 
         # Check if post.id is in df and set to True if df is empty.
         # This way new posts are still added to dictionary when df = ''
-            unique_id = post.id not in tuple(df.[id]) if csv_loaded else True
+            unique_id = post.id not in tuple(df.id) if csv_loaded else True
 
             # Save any unique posts to sub_dict.
             if unique_id:
@@ -67,3 +67,19 @@ class SubredditScrapper:
                 sub_dict['ups'].append(post.ups)
                 sub_dict['downs'].append(post.downs)
             sleep(0.1)
+
+
+            new_df = pd.DataFrame(sub_dict)
+
+            # Add new_df to df if df exists then save it to a csv.
+            if 'DataFrame' in str(type(df)) and self.mode == 'w':
+                pd.concat([df, new_df], axis=0, sort=0).to_csv(csv, index=False)
+                print(
+                    f'{len(new_df)} new posts collected and added to {csv}')
+            elif self.mode == 'w':
+                new_df.to_csv(csv, index=False)
+                print(f'{len(new_df)} posts collected and saved to {csv}')
+            else:
+                print(
+                    f'{len(new_df)} posts were collected but they were not '
+                    f'added to {csv} because mode was set to "{self.mode}"')
