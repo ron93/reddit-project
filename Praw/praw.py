d7b8c827351fc1c5833c78388ad5d6cb1defa52b
ron93 <ronmkare1@gmail.com>
2021-02-14 20:48:01 +0300

    get unique posts

--- Praw/praw.py
+++ Praw/praw.py
@@ -30,3 +30,40 @@ class SubredditScrapper:
             self.sort = 'hot'
             print('Sort method was not recognized, defaulting to hot.')
             return self.sort, reddit.subreddit(self.sub).hot(limit=self.lim)
+
+    def get_posts(self):
+        """get unique posts from a specified subreddit"""
+        sub_dict = {
+            'selftext':[],"title":[],"id":[],"sorted_by":[],"num_comments":[],"score" :[], "ups":[], "downs":[]
+        }
+        csv = f'{self.sub}_post.csv'
+        
+        #sorting method 
+        sort, subreddit = self.set_sort()
+
+        #set csv loaded to true 
+        df , csv_loaded = (pd.read_csv(csv), 1) if isfile(csv) else('',0)
+        
+        print(f'csv = {csv}')
+        print(f'After set_sort(), sort = {sort} and sub = {self.sub}')
+        print(f'csv_loaded = {csv_loaded}')
+
+        print(f'Collecting information from r/{self.sub}.')
+
+        for post in subreddit:
+
+        # Check if post.id is in df and set to True if df is empty.
+        # This way new posts are still added to dictionary when df = ''
+            unique_id = post.id not in tuple(df.[id]) if csv_loaded else True
+
+            # Save any unique posts to sub_dict.
+            if unique_id:
+                sub_dict['selftext'].append(post.selftext)
+                sub_dict['title'].append(post.title)
+                sub_dict['id'].append(post.id)
+                sub_dict['sorted_by'].append(sort)
+                sub_dict['num_comments'].append(post.num_comments)
+                sub_dict['score'].append(post.score)
+                sub_dict['ups'].append(post.ups)
+                sub_dict['downs'].append(post.downs)
+            sleep(0.1)
