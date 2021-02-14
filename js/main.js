8b84e1dded4b96b8487fe25aea7de46f8cda8f0c
ron93 <ronmkare1@gmail.com>
2021-02-14 22:42:30 +0300

    telreddit proj

--- js/main.js
+++
@@ -1,8 +0,0 @@
-
-jQuery(document).ready(function() {
-$( "#nav-toggle" ).on( "click", function() {
-  $( this ).toggleClass( "active" );
-    $('.pl-primary-nav').toggleClass("active");
-});
-
-});
