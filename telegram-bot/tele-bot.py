fe3032f54276043bcd32c81f0b3a8e872337b7b3
ron93 <ronmkare1@gmail.com>
2021-02-19 13:00:00 +0300

    reddit bot command - get pics from folder

--- telegram-bot/tele-bot.py
+++ telegram-bot/tele-bot.py
@@ -4,6 +4,7 @@ from  telegram.ext import CommandHandler, Updater, CallbackContext
 import re
 import yaml
 import os
+import csv
 
 #bot = telegram.Bot(token='1641440572:AAE6XodFCsJSgSKLdJcnUZZyvXUKirHq10k')
 
@@ -14,22 +15,39 @@ token=config['general']['token']
 
 bot = Bot(token)
 
-
+def get_reddit():
+    r = []
+    with open("../data/csv/earthporn_post.csv", "r") as f:
+        reader = csv.reader(f, delimiter="\t")
+        for i, line in enumerate(reader):
+            r.append(line)
+        return r
 
 def test(update:Update, context:CallbackContext):
     chat_id = update.effective_chat.id
-
+    
     bot.send_photo(chat_id=chat_id, photo=open('../data/piv.jpg', 'rb'))
 
+def reddits(update:Update,context:CallbackContext):
+    
+    r = get_reddit()
+    for i in r:
+        bot.send_message(
+            chat_id = update.effective_chat.id,
+            text = i
+         )
 
 def main():
     
     updater = Updater(token=config['general']['token'], use_context = True)
     dispacher = updater.dispatcher
     start_value = CommandHandler('motion', test)
+    red = CommandHandler('r', reddits)
+
     #dispacher.add_handler(CommandHandler('bop',bop))
     dispacher.add_handler(start_value)
- 
+    dispacher.add_handler(red)
+
     updater.start_polling()
     updater.idle()
 
